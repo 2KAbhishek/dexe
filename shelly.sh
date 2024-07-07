5a0ceb36bce0b81e236321ad39b1d768b5e3ddee
Abhishek Keshri <keshri.abhishek63@gmail.com>
2024-07-07 10:25:36 +0530

    Initial commit

---
+++ shelly.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+display_help() {
+    cat << EOF
+shelly: Publish CLI Tools 🐚✨
+
+Usage: shelly <required> [optional]
+
+Arguments:
+  required:        The required value
+  optional:        The optional value
+EOF
+}
+
+check_command() {
+    if ! command -v "$1" &> /dev/null; then
+        echo "Error: The $1 command is not available. Make sure it is installed."
+        exit 1
+    fi
+}
+
+main() {
+    DEFAULT_VALUE="sh"
+    required=$1
+    optional=${2:-$DEFAULT_VALUE}
+    echo "Required: $required, Optional: $optional"
+
+    check_command "$optional"
+
+    if [[ "$1" == "-h" || "$1" == "--help" ]]; then
+        display_help
+        exit 0
+    fi
+
+    if [ $# -lt 1 ]; then
+        echo "Error: required is required."
+        echo
+        display_help
+        exit 1
+    fi
+}
+
+main "$@"
+
